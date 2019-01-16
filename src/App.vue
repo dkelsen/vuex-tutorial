6c5ea99df09de95bbdb67b80445903b835884702
Dilshan Kelsen <dilshan.kelsen@gmail.com>
2019-01-16 19:04:23 +0100

    preparations

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <counter class="center"></counter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Counter from "./components/Counter";
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    counter: Counter
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.center {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <button>Increment</button>
+    <button>Decrement</button>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
